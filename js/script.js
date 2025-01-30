1d50c3014ac3662dbc486e4a13f439e57bca4885
Astridisbach <astridisbachsorensen@gmail.com>
2025-01-30 12:30:28 +0100

    .

--- js/script.js
+++ js/script.js
@@ -9,3 +9,7 @@ function onBulb() {
     const bulb = document.getElementById("bulb");
     bulb.src = "img/off.jpg";
   }
+
+  document.getElementById("onBtn").addEventListener('click', onBulb);
+
+  document.getElementById("offBtn").addEventListener('click', offBulb);
